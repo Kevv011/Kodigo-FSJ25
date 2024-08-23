2d6f82a074e1c1a474f96271f0521a0731230bad
KevArevalo026 <kevinarepalacios123@gmail.com>
2024-08-22 21:10:54 -0600

    Se vio la teoria e introduccion a Javascript

--- Javascript/main.js
+++ Javascript/main.js
@@ -1,1 +1,25 @@
 {}
+//COMENTAR EN UNA LINEA -> DESACTIVAR UNA LINEA PARA EL ENTORNO DE EJECUCION
+
+/*  
+    COMENTARIO MULTILINEA
+    COMENTARIO MULTILINEA
+    COMENTARIO MULTILINEA
+*/ 
+
+/*
+    CARACTERISTICAS DE JAVASCRIPT
+    • Tipo de tipado: Debilmente Tipado  -->  Se perdonan errores y no se agregan algunos errores
+    • Tipado debil, no necesita:
+        - CIERRES DE INSTRUCCION
+        - ACLARAR EL TIPO DE DATOS DE LAS VARIABLES
+
+    • PARADIGMA DE PROGRAMACION
+        - Programacion estructurada (Trabaja por funciones)
+
+    • JAVASCRIPT ES UN LENGUAJE DE PROGRAMACION INTERPRETADO (Ejecutado en el navegador)
+    
+
+
+
+*/ 
